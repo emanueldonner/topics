5381b15d7a0102aa00b1878f3c552b2d231ae99a
emanueldonner <emanuel.donner@wh-d.at>
2024-06-24 17:57:52 +0200

    Removed falsly imported cheerio

--- src/routes/d/+page.svelte
+++ src/routes/d/+page.svelte
@@ -1,12 +1,11 @@
 <script>
 	import Icon from '@iconify/svelte';
-	import { add } from 'cheerio/lib/api/traversing';
 </script>
 
 <h2>Topics</h2>
 
 <div class="container">
-	<button class="add-topic-button" on:click={() => add}>
+	<button class="add-topic-button" on:click={() => console.log('add topic')}>
 		<Icon icon="mdi:plus" />
 	</button>
 </div>
